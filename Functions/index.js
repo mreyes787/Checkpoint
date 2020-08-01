d07d0db3c57a067c3685471beb99c11ff2e84ea3
Maricela Patrickios <mreyes777@gmail.com>
2020-07-31 20:23:58 -0400

    Functions were easy

--- Functions/index.js
+++ Functions/index.js
@@ -17,7 +17,9 @@ CREATE A FUNCTION THAT RETURNS THE BOOLEAN true
 
 */
 
-function example() {}
+function example() {
+  return true
+}
 
 /*
 QUESTION 1
@@ -27,7 +29,7 @@ CREATE A FUNCTION THAT RETURNS THE BOOLEAN false
 */
 
 function firstFunction() {
-  // YOUR CODE HERE
+  return false;
 }
 
 /*
@@ -38,7 +40,7 @@ CREATE A FUNCTION THAT RETURNS THE STRING 'hello world'
 */
 
 function helloWorld() {
-  // YOUR CODE HERE
+ return "hello world";
 }
 
 /*
@@ -54,7 +56,7 @@ YOU WILL HAVE TO EDIT THE FUNCTION DECLARATION (the line where the key word 'fun
 */
 
 function question3(param) {
-  // YOUR CODE HERE
+  return param;
 }
 
 /*
@@ -69,7 +71,7 @@ concatQuestion('blake') ===> 'hello blake'
 */
 
 function concatQuestion(str) {
-  // YOUR CODE HERE
+  return "hello " + str;
 }
 
 /*
@@ -85,7 +87,7 @@ subtraction(1, 2) ===> -1
 */
 
 function subtraction(num1, num2) {
-  // YOUR CODE HERE
+  return num1 - num2;
 }
 
 /*
@@ -101,7 +103,7 @@ multiplication(2, 0) ===> 0
 */
 
 function multiplication(num1, num2) {
-  // YOUR CODE HERE
+  return num1 * num2;
 }
 
 /*
@@ -117,7 +119,7 @@ square(4) ===> 16
 */
 
 function square(num) {
-  // YOUR CODE HERE
+  return num * num; 
 }
 
 /*
@@ -134,7 +136,7 @@ e.g.
 var answerToQuestion8;
 
 function question8() {
-  // YOUR CODE HERE
+  return "we can save return values in variables";
 }
 
 answerToQuestion8 = question8();
